b9c8599ea8decab6a1479715f420a4482bd1b346
mrchazaaa <charlie.howlett@live.co.uk>
2019-11-05 01:08:40 +0000

    updated README with TODO and also started modularizing monkebot. added basic middlewar archetecture for handling requests, that utilises a custom exception, but could be improved. Possibly by chaining.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 checkout:
 https://fbchat.readthedocs.io/en/stable/intro.html
+
+TO DO:
+[x] reply to messages on mention
+ [x] react to messages that mention Emanuel
+ [] 'stinky' => reply with random fart recording
+ [] 'meme' => reply with random meme
+  [] images mentioning emanuel will be added to meme database
+ [] default respond with random phrase
+  [] allow users to add phrases to database
+[] respond to messages randomly (every now and then, not often)?
+[] send good morning/evening message to gc every morning/evening
+[] send gifs
+
+[] NEED TO MAKE SURE THAT MONKEBOT LOGS OUT SAFELY 
--- script.py
+++ script.py
@@ -1,4 +1,3 @@
-
 import os
 import jsonReader
 from fbchat import Client
@@ -12,39 +11,58 @@ PASSWORD = jsonReader.getValue('PASSWORD')
 
 message = ['']
 
+# Custom exception thrown during middleware execution to indicate that no further middleware should be processed
+class BreakMiddlewareChain(Exception):
+   def __init__(self, arg):
+      self.args = arg
+
 # Subclass fbchat.Client and override required methods
 class MonkeBot(Client):
     global message
 
     def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
-        # self.mark_as_delivered(thread_id, message_object.uid)
-        # self.mark_as_read(thread_id)
-
+        
         print(f"my uid is {self.uid}")
-
         print("{} from {} in {}".format(message_object, thread_id, thread_type.name))
 
-        # If you're not the author, echo
-        # if author_id != self.uid:
-            # print("sending reply")
-            # self.send(message_object, thread_id=thread_id, thread_type=thread_type)
+        #only reply to messages that: 
+        # - mention Emanuel
+        # - are DMs sent by charlie howlett (for testing)
+        # - are not sent by Emanuel (do not respond to your own messages)
+        if ((next((x for x in message_object.mentions if x.thread_id == self.uid), None) != None or
+            author_id == CHARLIES_ID) and 
+            author_id != self.uid):
 
-        # if (author_id == CHARLIES_ID):
-            # self.sendLocalVoiceClips('./sounds/Fart-Squeeze-Yer-Knees_Mike-Koenig.mp3', Message(text="uh oh"), thread_id=thread_id, thread_type=thread_type)
-            # self.reactToMessage(message_object.uid, MessageReaction.SMILE)
-        # else:
-            # self.reactToMessage(message_object.uid, MessageReaction.NO)
-        # if ("Emanuel" in message_object.text):
-        # if (self.uid in message_object.mentions):
+            try:
+                # React to sender message
+                self.reactToMessage(message_object.uid, MessageReaction.SMILE)
 
-        #only reply to message if monkebot is mentioned
-        if (next((x for x in message_object.mentions if x.thread_id == self.uid), None) != None):
+                self.recordings(message_object, thread_id, thread_type)
+
+                # Defines default behaviour so must be last (could change default behaviour to seperate method)
+                self.phrases(message_object, thread_id, thread_type)
+            except BreakMiddlewareChain:
+                return
+
+    
+    def phrases(self, message_object, thread_id, thread_type):
+        #check to see if phrases are being added
+        if ("add phrase" in message_object.text):
+            #add phrase to database
+            phraseToAdd = message_object.text.split("add phrase", 1)[1]
+            raise BreakMiddlewareChain
+        #check to see if phrases are being requested (current default behaviour, should probably refactor)
+        else:
+            messagesFromDB = ["UH OH", "STINKY", "POOPY", "HAHA"] #replace with DB call
+            self.send(Message(text=random.choice(messagesFromDB)), thread_id=thread_id, thread_type=thread_type)
+
+    def recordings(self, message_object, thread_id, thread_type):
+        #check to see if a recording is being added
+        #check to see if a recording is being requested
+        if ("stinky" in message_object.text):
+            # send fart noise
             self.sendLocalVoiceClips('./sounds/Fart-Squeeze-Yer-Knees_Mike-Koenig.mp3', Message(text="uh oh"), thread_id=thread_id, thread_type=thread_type)
-
-            # self.sendLocalVoiceClips('./sounds/Fart-Squeeze-Yer-Knees_Mike-Koenig.mp3', Message(text="uh oh"), thread_id=thread_id, thread_type=thread_type)
-            # self.send(Message(text=random.choice(message)), thread_id=thread_id, thread_type=thread_type)
+            raise BreakMiddlewareChain
 
 client = MonkeBot(USERNAME, PASSWORD)
-
 client.listen()
-
